87c93227af9b572f1ed1825f099a14279ca4de4d
Zachary Sluder <zachary.sluder@gmail.com>
2019-09-26 08:22:51 -0400

    init

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,13 +4,5 @@ namespace App\Http\Controllers;
 
 class PageController extends Controller
 {
-    /**
-     * Initial page for site
-     *
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
-    public function index()
-    {
-        return view('welcome');
-    }
+   
 }
